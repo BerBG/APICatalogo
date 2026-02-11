2e57e05d8018a94134b8a996bc1f07f80a67d14b
Bernardo <bernardo.portes@inpartsaude.com.br>
2026-02-10 23:56:29 -0300

    feat: Criando o controlador ProdutosController

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly APICatalogoContext _context;
+
+        public ProdutosController(APICatalogoContext context)
+        {
+            _context = context;
+        }
+    }
+}
